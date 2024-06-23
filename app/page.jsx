f447c11c893f837c0f1cf561e6981474a3ddbe2c
Arnab Dev <official.arnabdev@gmail.com>
2024-06-23 09:12:32 +0600

    format

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <section className="h-full">
+    <section className="h-full mb-20">
       <div className="container w-full h-full mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 ">
           <div className="text-center xl:text-left order-2 xl:order-none">
@@ -31,31 +31,32 @@ const Home = () => {
                 technologies.
               </div> */}
             </p>
-            <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Link href="cv.pdf">
-                <Button
-                  variant="outline"
-                  size="lg"
-                  className="uppercase flex  items-center gap-2 "
-                >
-                  <span>Download CV</span>
-                  <FiDownload className="text-xl " />
-                </Button>
-              </Link>
-              <div className="mb-8 xl:mb-0">
-                <Social
-                  containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-[#cbacf9] rounded-full flex justify-center items-center text-[#cbacf9] text-base hover:bg-[#cbacf9] hover:text-primary hover:transition-all duration-primary"
-                />
-              </div>
-            </div>
+            <Stats/>
           </div>
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
-      <Stats />
+      
+      <div className="flex flex-col xl:flex-row items-center justify-center gap-8">
+        <Link href="cv.pdf">
+          <Button
+            variant="outline"
+            size="lg"
+            className="uppercase flex  items-center gap-2 "
+          >
+            <span>Download CV</span>
+            <FiDownload className="text-xl " />
+          </Button>
+        </Link>
+        <div className="mb-8 xl:mb-0">
+          <Social
+            containerStyles="flex gap-6"
+            iconStyles="w-9 h-9 border border-[#cbacf9] rounded-full flex justify-center items-center text-[#cbacf9] text-base hover:bg-[#cbacf9] hover:text-primary hover:transition-all duration-primary"
+          />
+        </div>
+      </div>
     </section>
   );
 };
